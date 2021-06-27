3f3dde7f78b6cad11f68a21b656eb024c709dc8a
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-06-27 23:01:03 +0300

    Add description

--- WeatherLight/Extention/ViewController+alertController.swift
+++ WeatherLight/Extention/ViewController+alertController.swift
@@ -11,8 +11,8 @@ extension ViewController {
     func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style, completionHandler: @escaping (String) -> Void) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
         alertController.addTextField { tf in
-            let cities = ["Moscow", "Minsk", "Kiev", "Warsaw", "Vilnius", "Riga"]
-            tf.placeholder = cities.randomElement()
+            let cities = ["Moscow", "Minsk", "Kiev", "Warsaw", "Vilnius", "Riga", "New York", "Berlin", "Tokyo", "Los Angeles", "Madrid"]
+            tf.placeholder = "Please, enter city (e.g.: \(cities.randomElement() ?? "Minsk"))"
         }
         let search = UIAlertAction(title: "Search", style: .default) { action in
             let textField = alertController.textFields?.first
